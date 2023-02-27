46cdecff60b2d7832f436b980b0ef8e25bef20b8
ItzTJSV2 <maxtran7777@gmail.com>
2023-02-27 17:35:22 +0700

    Max's Commit (27/2/23)

---
+++ Database Calls/UpdateFound.py
@@ -0,0 +1,18 @@
+import sqlite3
+from GetItems import *
+from datetime import datetime
+
+conn = sqlite3.connect('Database.db')
+cursor = conn.cursor()
+
+current_time = datetime.today()
+
+for item in GetAllItems():
+    time_diff = datetime.today() - datetime.strptime(item[4], "%Y-%m-%d")
+    if (time_diff >= 10):
+        ItemIsNowFree(item[0])
+
+
+
+conn.commit()
+conn.close()
